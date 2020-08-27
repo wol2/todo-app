80bd9f3ef60df6e0e0267b5d10d0629be524c911
devwol2 <parkwol2@naver.com>
2020-08-27 23:22:21 +0900

    update_2008-08-27

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import Todoinsert from './components/Todoinsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [Todos, setTodos] = useState([
+    { id: 1, text: '리엑트의 기초 알아보기', checked: true },
+    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
+    { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(Todos.concat(todo));
+      nextId.current += 1;
+    },
+    [Todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(Todos.filter((todo) => todo.id !== id));
+    },
+    [Todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        Todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [Todos],
+  );
+
   return (
     <TodoTemplate>
-      <Todoinsert />
-      <TodoList />
+      <Todoinsert onInsert={onInsert} />
+      <TodoList todos={Todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
--- src/components/Todoinsert.js
+++ src/components/Todoinsert.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './Todoinsert.scss';
 
-const Todoinsert = () => {
+const Todoinsert = ({ onInsert }) => {
+  const [Value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(Value);
+      setValue('');
+      e.preventDefault();
+    },
+    [onInsert, Value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요." />
-      <button type="submit">
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요."
+        value={Value}
+        onChange={onChange}
+      />
+      <button type="submit" onClick={onSubmit}>
         <MdAdd />
       </button>
     </form>
